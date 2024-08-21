512091fab71776b56283d2df79721a248b342940
salmaghabri <salma.ghabri@insat.ucar.tn>
2024-08-21 15:09:46 +0100

    basique

--- main.ts
+++ main.ts
@@ -1,89 +1,66 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	// Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface TogglePluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: TogglePluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TogglePlugin extends Plugin {
+	settings: TogglePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addCommand({
+			id: "toggle-link-format-command",
+			name: "Toggle link format command",
+			hotkeys: [{ modifiers: ["Mod", "Shift"], key: "q" }],
+			editorCheckCallback: (
+				checking: boolean,
+				editor: Editor,
+				view: MarkdownView
+			) => {
+				const sel = editor.getSelection();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+				if (isValidMarkdownOrWikiLink(sel)) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						console.log(`You have selected: ${sel}`);
+						console.log(
+							`Toggled text: ${toggleWikiLinkAndMarkdown(sel)}`
+						);
+						editor.replaceSelection(toggleWikiLinkAndMarkdown(sel));
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+
+				return false;
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +68,83 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: TogglePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TogglePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
+}
+
+// ************************ utils ************************
+function toggleWikiLinkAndMarkdown(text: string) {
+	if (/\!\[.*?\]\(.*?\)/.test(text) || /\[.*?\]\(.*?\)/.test(text)) {
+		// convert Markdown to WikiLink
+		text = text.replace(/\!\[(.*?)\]\((.*?)\)/g, (match, altText, url) => {
+			const decodedUrl = decodeURIComponent(url.trim());
+			return `![[${decodedUrl}|${altText.trim()}]]`;
+		});
+		text = text.replace(/\[(.*?)\]\((.*?)\)/g, (match, linkText, url) => {
+			const decodedUrl = decodeURIComponent(url.trim());
+			return `[[${decodedUrl}|${linkText.trim()}]]`;
+		});
+	} else {
+		// convert WikiLink to Markdown
+		text = text.replace(
+			/!\[\[(.*?)(\|(.*?))?\]\]/g,
+			(match, url, pipe, altText) => {
+				const encodedUrl = encodeURI(url.trim());
+				return `![${(altText || url).trim()}](${encodedUrl})`;
+			}
+		);
+		text = text.replace(
+			/\[\[(.*?)(\|(.*?))?\]\]/g,
+			(match, url, pipe, linkText) => {
+				const encodedUrl = encodeURI(url.trim());
+				return `[${(linkText || url).trim()}](${encodedUrl})`;
+			}
+		);
+	}
+	return text;
+}
+
+function isValidMarkdownOrWikiLink(text: string): boolean {
+	//  ![alt text](url)
+	const markdownImagePattern = /!\[[^\[\]]*?\]\([^\(\)]+?\)/;
+
+	// [text](url)
+	const markdownLinkPattern = /\[[^\[\]]*?\]\([^\(\)]+?\)/;
+
+	//  ![[filename]]
+	const wikiImagePattern = /!\[\[[^\[\]]+?\]\]/;
+
+	// [[filename]]
+	const wikiLinkPattern = /\[\[[^\[\]]+?\]\]/;
+
+	return (
+		markdownImagePattern.test(text) ||
+		markdownLinkPattern.test(text) ||
+		wikiImagePattern.test(text) ||
+		wikiLinkPattern.test(text)
+	);
 }
